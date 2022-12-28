0a1d96fb11b724b1702e110da7d6582ee808c328
NVZ1994 <nikita.zhuk.1994@gmail.com>
2022-12-28 14:52:40 +0200

    task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ul = document.getElementById('ingredients');
+console.log(ul)
+
+ingredients.forEach(element => {
+  const item = document.createElement('li');
+  item.textContent = element;
+  item.classList.add('item');
+
+  ul.appendChild(item)
+})
+
