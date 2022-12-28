f23e44990e9807e7783340c4b18c8235eadec2d3
NVZ1994 <nikita.zhuk.1994@gmail.com>
2022-12-28 21:14:32 +0200

    all tasks

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,35 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const createBtn = document.querySelector('button[data-create]'); 
+const destroyBtn = document.querySelector('button[data-destroy]'); 
+const input = document.querySelector('#controls > input')
+const boxes = document.getElementById('boxes')
+
+
+const createBoxes = amount => {
+    const elementsToAdd = []
+    for (let i = 0; i < amount; i += 1) {
+        const div = document.createElement('div')
+        div.style.height = `${30 + 10 * i}px`
+        div.style.width = `${30 + 10 * i}px`
+        div.style.background = getRandomHexColor()
+        div.textContent = getRandomHexColor()
+        elementsToAdd.push(div)
+    }
+    return elementsToAdd
+}
+const destroyBoxes = () => {
+    boxes.innerHTML = ''
+}
+createBtn.addEventListener('click', () => {
+    let boxesToAdd = createBoxes(input.value)
+    boxes.append(...boxesToAdd)
+})
+console.log(input.value)
+destroyBtn.addEventListener('click', () => {
+    destroyBoxes.call()
+})
+
+// сделал не сам )=
