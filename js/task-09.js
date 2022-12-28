7f738cab3d77597e1a39c9aceafa0b14d06ad24c
NVZ1994 <nikita.zhuk.1994@gmail.com>
2022-12-28 20:21:15 +0200

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,18 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+// Напиши скрипт, который изменяет цвета фона элемента <body> через инлайн стиль при клике на
+//  button.change-color и выводит значение цвета в span.color.
+
+const btn = document.querySelector('.change-color')
+const span = document.querySelector('.color')
+
+btn.addEventListener('click', colorChanger)
+
+function colorChanger() {
+  const body = document.body;
+  body.style.backgroundColor = getRandomHexColor()
+  span.textContent = getRandomHexColor()
+}
